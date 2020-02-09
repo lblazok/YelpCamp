da0fa72231e696d43e5b2777919ef37057607a0c
lblazok <lblazok3@gmail.com>
2020-02-09 11:18:37 +0100

    1st real commit

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require("express")
+const app = express()
+const bodyParser = require("body-parser")
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs")
+
+let campgrounds = [
+    {name: "Salmon Creek", image: "https://images.unsplash.com/photo-1523987355523-c7b5b0dd90a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"},
+    {name: "Granite Hill", image: "https://images.unsplash.com/photo-1486915309851-b0cc1f8a0084?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"},
+    {name: "Mountain Goat's Rest", image: "https://images.unsplash.com/photo-1510312305653-8ed496efae75?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=667&q=80"}
+]
+
+app.get("/", (req, res) => {
+    res.render("landing")
+})
+
+
+app.get("/campgrounds", (req, res) => {
+   
+    res.render("campgrounds", {campgrounds: campgrounds})
+})
+
+app.get("/campgrounds/new", (req, res) => {
+    res.render("new")
+})
+
+app.post("/campgrounds", (req, res) => {
+    // get data form form and add data to campgrounds array
+    let name = req.body.name
+    let image = req.body.image
+    let newCampground = {name: name, image: image}
+    campgrounds.push(newCampground)
+    // redirect back to campgrounds array
+    res.redirect("/campgrounds")
+})
+
+
+app.listen(3000, () => {
+    console.log("Server is listening on port 3000")
+})
